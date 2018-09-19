f86384c482f065d228b462da4c88d381c3755af3
Nikesh Shetty <nikesh_shetty@external.mckinsey.com>
2018-09-19 12:11:28 +0530

    first commit

---
+++ src/test/java/model/InputOutputKtTest.kt
@@ -0,0 +1,12 @@
+package model
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class InputOutputKtTest {
+
+    @Test
+    fun calculateSpeed() {
+    }
+}
